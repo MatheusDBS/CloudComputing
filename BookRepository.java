1513c11f838d062804a399b5005141201b392e03
Matheus <matheusdebortolisilva@gmail.com>
2026-09-22 18:32:05 -0300

    fix

--- BookRepository.java
+++
@@ -1,7 +0,0 @@
-package br.bookgather.bookservice.repository;
-
-import br.bookgather.bookservice.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
